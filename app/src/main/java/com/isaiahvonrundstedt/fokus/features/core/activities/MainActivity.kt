b16219a08ad7123adb1096aadb043707ab038617
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2020-06-12 19:27:19 +0800

    Fixed crashing when opening the menu

--- app/src/main/java/com/isaiahvonrundstedt/fokus/features/core/activities/MainActivity.kt
+++ app/src/main/java/com/isaiahvonrundstedt/fokus/features/core/activities/MainActivity.kt
@@ -1,26 +1,15 @@
 package com.isaiahvonrundstedt.fokus.features.core.activities
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.isaiahvonrundstedt.fokus.R
-import com.isaiahvonrundstedt.fokus.features.about.AboutActivity
-import com.isaiahvonrundstedt.fokus.features.notifications.NotificationActivity
-import com.isaiahvonrundstedt.fokus.features.settings.SettingsActivity
-import com.isaiahvonrundstedt.fokus.features.shared.PreferenceManager
+import com.isaiahvonrundstedt.fokus.components.bottomsheet.NavigationBottomSheet
 import com.isaiahvonrundstedt.fokus.features.shared.abstracts.BaseActivity
-import com.isaiahvonrundstedt.fokus.features.shared.abstracts.BaseBottomSheet
-import com.isaiahvonrundstedt.fokus.features.shared.adapters.NavigationAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.layout_appbar.*
-import kotlinx.android.synthetic.main.layout_sheet_navigation.*
 
 class MainActivity: BaseActivity() {
 
@@ -34,9 +23,8 @@ class MainActivity: BaseActivity() {
 
         toolbar?.navigationIcon = ContextCompat.getDrawable(this, R.drawable.ic_menu)
         toolbar?.setNavigationOnClickListener {
-            NavigationBottomSheet()
-                .invoke(supportFragmentManager)
-        }
+            NavigationBottomSheet().invoke(supportFragmentManager)
+    }
 
         val navigationHost = supportFragmentManager.findFragmentById(R.id.navigationHostFragment)
         controller = navigationHost?.findNavController()
@@ -62,34 +50,5 @@ class MainActivity: BaseActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean = true
-
-    private class NavigationBottomSheet: BaseBottomSheet(), NavigationAdapter.NavigationListener {
-        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                                  savedInstanceState: Bundle?): View? {
-            return inflater.inflate(R.layout.layout_sheet_navigation, container, false)
-        }
-
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
-
-            menuTitleView.text = String.format(getString(R.string.hello),
-                PreferenceManager(context).name)
-
-            val adapter = NavigationAdapter(activity, this)
-            adapter.setItems(R.menu.menu_main)
-            recyclerView.layoutManager = LinearLayoutManager(context)
-            recyclerView.adapter = adapter
-        }
-
-        override fun onNavigate(id: Int) {
-            when (id) {
-                R.id.action_notifications -> startActivity(
-                    Intent(context, NotificationActivity::class.java))
-                R.id.action_settings -> startActivity(Intent(context, SettingsActivity::class.java))
-                R.id.action_about -> startActivity(Intent(context, AboutActivity::class.java))
-            }
-            this.dismiss()
-        }
-    }
 
 }

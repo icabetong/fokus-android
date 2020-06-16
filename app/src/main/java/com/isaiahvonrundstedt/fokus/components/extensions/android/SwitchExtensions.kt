fbe9edede03575fea1c4a7e389695b2d36946fdd
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2020-06-16 14:03:13 +0800

    Added Custom Switch Drawable

--- app/src/main/java/com/isaiahvonrundstedt/fokus/components/extensions/android/SwitchExtensions.kt
+++ app/src/main/java/com/isaiahvonrundstedt/fokus/components/extensions/android/SwitchExtensions.kt
@@ -1,17 +1,17 @@
 package com.isaiahvonrundstedt.fokus.components.extensions.android
 
-import android.widget.Switch
 import androidx.annotation.ColorRes
+import androidx.appcompat.widget.SwitchCompat
 import androidx.core.content.ContextCompat
 import com.isaiahvonrundstedt.fokus.R
 
-fun Switch.changeTextColorWhenChecked() {
+fun SwitchCompat.changeTextColorWhenChecked() {
     this.setOnCheckedChangeListener { _, isChecked ->
         if (isChecked) this.setTextColorFromResource(R.color.colorPrimaryText)
         else this.setTextColorFromResource(R.color.colorSecondaryText)
     }
 }
 
-fun Switch.setTextColorFromResource(@ColorRes id: Int) {
+fun SwitchCompat.setTextColorFromResource(@ColorRes id: Int) {
     this.setTextColor(ContextCompat.getColor(this.context, id))
 }

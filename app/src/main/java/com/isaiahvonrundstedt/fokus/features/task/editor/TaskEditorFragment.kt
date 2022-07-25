4777df25a5e3c2ae1017ef04ea722970cb7613a8
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2022-07-25 09:44:57 +0800

    Added DynamicTheming

--- app/src/main/java/com/isaiahvonrundstedt/fokus/features/task/editor/TaskEditorFragment.kt
+++ app/src/main/java/com/isaiahvonrundstedt/fokus/features/task/editor/TaskEditorFragment.kt
@@ -35,7 +35,7 @@ import com.afollestad.materialdialogs.utils.MDUtil.textChanged
 import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
-import com.isaiahvonrundstedt.fokus.CoreApplication
+import com.isaiahvonrundstedt.fokus.Fokus
 import com.isaiahvonrundstedt.fokus.R
 import com.isaiahvonrundstedt.fokus.components.extensions.android.*
 import com.isaiahvonrundstedt.fokus.components.extensions.jdk.toCalendar
@@ -584,7 +584,7 @@ class TaskEditorFragment : BaseEditorFragment(), FragmentResultListener {
                         createSnackbar(R.string.feedback_export_completed, binding.root)
 
                         intent.getStringExtra(BaseService.EXTRA_BROADCAST_DATA)?.also {
-                            val uri = CoreApplication.obtainUriForFile(requireContext(), File(it))
+                            val uri = Fokus.obtainUriForFile(requireContext(), File(it))
 
                             startActivity(
                                 ShareCompat.IntentBuilder.from(requireActivity())
@@ -782,7 +782,7 @@ class TaskEditorFragment : BaseEditorFragment(), FragmentResultListener {
                         ImageViewer.show(childFragmentManager, file.path, attachment.name)
                     }
                     else -> {
-                        val targetUri = CoreApplication.obtainUriForFile(
+                        val targetUri = Fokus.obtainUriForFile(
                             requireContext(),
                             file
                         )
